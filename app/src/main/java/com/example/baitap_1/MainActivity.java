f18152e11bdf2ab3f392a35cb7aeef30aad8e907
anduo <123@>
2025-02-15 22:14:52 +0700

    thêm giao diện

--- app/src/main/java/com/example/baitap_1/MainActivity.java
+++ app/src/main/java/com/example/baitap_1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.baitap_1;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edtCanh;
+    Button btnTinh, btnWeb;
+    TextView chuvi, dientich;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        edtCanh = findViewById(R.id.edtCanh);
+        btnTinh = findViewById(R.id.btnTinh)
     }
 }
